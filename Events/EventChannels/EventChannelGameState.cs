d7714119eb65f02d8b3b2bb4c4bd93bd40c60e93
Osman Caner Nurdag <51195463+canernurdag@users.noreply.github.com>
2022-10-01 19:07:32 +0300

    +Add Scriptable Object Event classess

---
+++ Events/EventChannels/EventChannelGameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Tolian.ScriptableSystem;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventChannelGameState", menuName = "TolianGames/Event/EventChannelGameState")]
+public class EventChannelGameState : EventChannel<GameStateType>
+{
+   
+}
---
+++ Events/EventChannels/EventChannelVoid.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Tolian.ScriptableSystem;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventChannelVoid", menuName = "TolianGames/Event/EventChannelVoid")]
+public class EventChannelVoid : EventChannel<Void>
+{
+   
+}
